e242cbd5fe4ea2ca2cd23c69a497a3753ee92d90
UKulsum <ukulsum1280@gmail.com>
2024-01-25 12:02:49 +0600

    ds

--- Controllers/AgentHomeController.cs
+++ Controllers/AgentHomeController.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using USBDProperty.Models;
 
 namespace USBDProperty.Controllers
 {
     public class AgentHomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AgentHomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View("Dashboard");
         }
 
-        public IActionResult AgentIndex()
+        public IActionResult AgentIndex(int id)
         {
-            return View();
+            var data = _context.DevelopersorAgent.Where(p => p.ID == id).ToList();
+
+            return View(data);
         }
     }
 }
