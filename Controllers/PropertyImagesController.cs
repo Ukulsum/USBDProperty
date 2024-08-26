c35b87183c0c991f7648823091c10a78b2132d7f
Shahanaj <fashionfuel267@gmail.com>
2024-08-26 10:32:56 +0600

    pppp

--- Controllers/PropertyImagesController.cs
+++ Controllers/PropertyImagesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using NuGet.Packaging.Signing;
+//using NuGet.Packaging.Signing;
 using USBDProperty.Models;
 
 namespace USBDProperty.Controllers
